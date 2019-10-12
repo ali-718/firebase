afed9a62f6260217144c528874288d87c65d3b93
System Administrator <root@Alis-MacBook-Pro.local>
2019-10-12 19:45:23 +0500

    firebase

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./config";
+import * as f from "firebase";
+import { isTSEnumMember } from "@babel/types";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  state = {
+    users: []
+  };
+
+  // name = {name:ali}
+  // age = {age:21}
+  // data = {...this.name,...this.age}
+
+  componentDidMount() {
+    // f.database()
+    //   .ref("users")
+    //   .push({
+    //     name: "sumaim",
+    //     age: 21
+    //   });
+
+    f.database()
+      .ref("users")
+      .on("value", res => {
+        // console.log(res.val());
+        res.forEach(item => {
+          this.state.users.push({ id: item.key, ...item.val() });
+          // console.log(item.key);
+          // console.log(item.val());
+        });
+      });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>firebase</h1>
+        {console.log(this.state)}
+      </div>
+    );
+  }
 }
-
-export default App;
---
+++ src/config.js
@@ -0,0 +1,13 @@
+import * as firebase from "firebase";
+
+var firebaseConfig = {
+  apiKey: "AIzaSyAFcr_lHtriqe_9Q3n1Z_UjQtlEjQbg42I",
+  authDomain: "sumaim.firebaseapp.com",
+  databaseURL: "https://sumaim.firebaseio.com",
+  projectId: "sumaim",
+  storageBucket: "sumaim.appspot.com",
+  messagingSenderId: "1095154488968",
+  appId: "1:1095154488968:web:c5123248b9c65871e8313d"
+};
+
+firebase.initializeApp(firebaseConfig);
