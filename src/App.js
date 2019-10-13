ff49e5e0ccfb93cbcb0187294d9662985b22857f
ali-718 <alimurtuza718@gmail.com>
2019-10-13 14:00:24 +0500

    firebase

--- src/App.js
+++ src/App.js
@@ -1,43 +1,115 @@
-import React, { Component } from "react";
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 import "./config";
-import * as f from "firebase";
-import { isTSEnumMember } from "@babel/types";
+import * as firebase from "firebase";
+import { throwStatement } from "@babel/types";
 
-export default class App extends Component {
+class App extends React.Component {
   state = {
-    users: []
+    data: [],
+    name: "",
+    age: ""
   };
 
-  // name = {name:ali}
-  // age = {age:21}
-  // data = {...this.name,...this.age}
-
   componentDidMount() {
-    // f.database()
-    //   .ref("users")
-    //   .push({
-    //     name: "sumaim",
+    // pushing data in firebase
+    // firebase
+    //   .database()
+    //   .ref("tb_users")
+    //   .child("01")
+    //   .set({
+    //     name: "ali haider",
     //     age: 21
     //   });
+    // firebase
+    //   .database()
+    //   .ref("tb_users")
+    //   .push({
+    //     name: "Zeeshan",
+    //     age: 21
+    //   });
+    // fetching data in firebase
+    // firebase
+    //   .database()
+    //   .ref("tb_users")
+    //   .on("child_added", snapshot => {
+    //     // console.log(snapshot.val());
+    //     snapshot.forEach(res => {
+    //       // console.log(res.val());
+    //       this.state.data.push({
+    //         id: res.key,
+    //         ...res.val()
+    //       });
+    //       // console.log(res.key);
+    //       console.log(this.state);
+    //     });
+    //   });
 
-    f.database()
-      .ref("users")
-      .on("value", res => {
-        // console.log(res.val());
-        res.forEach(item => {
-          this.state.users.push({ id: item.key, ...item.val() });
-          // console.log(item.key);
-          // console.log(item.val());
-        });
-      });
+    // update data
+    firebase
+      .database()
+      .ref("tb_users")
+      .child("-Lr3IVftNOEMP4ejtvq2")
+      .update({ age: 22 });
+
+    // remove data
+    // firebase
+    // .database()
+    // .ref("tb_users")
+    // .child("-Lr3IVftNOEMP4ejtvq2")
+    // .remove();
   }
+
+  Submit = e => {
+    e.preventDefault();
+
+    let isValid = false;
+
+    firebase
+      .database()
+      .ref("tb_users")
+      .once("value")
+      .then(snapshot => {
+        snapshot.forEach(item => {
+          if (item.val().name == this.state.name) {
+            isValid = true;
+          }
+        });
+      })
+      .then(() => {
+        if (isValid) {
+          alert("duplcate name");
+        } else {
+          firebase
+            .database()
+            .ref("tb_users")
+            .push({
+              name: this.state.name,
+              age: this.state.age
+            });
+        }
+      });
+  };
 
   render() {
     return (
       <div>
-        <h1>firebase</h1>
-        {console.log(this.state)}
+        <form onSubmit={e => this.Submit(e)}>
+          <input
+            placeholder="name"
+            onChange={e => this.setState({ name: e.target.value })}
+          />
+          <input
+            placeholder="age"
+            onChange={e => this.setState({ age: e.target.value })}
+            value={this.state.age}
+          />
+          <input type="submit" />
+        </form>
       </div>
     );
   }
 }
+
+export default App;
--- src/config.js
+++ src/config.js
@@ -1,13 +1,13 @@
 import * as firebase from "firebase";
 
 var firebaseConfig = {
-  apiKey: "AIzaSyAFcr_lHtriqe_9Q3n1Z_UjQtlEjQbg42I",
-  authDomain: "sumaim.firebaseapp.com",
-  databaseURL: "https://sumaim.firebaseio.com",
-  projectId: "sumaim",
-  storageBucket: "sumaim.appspot.com",
-  messagingSenderId: "1095154488968",
-  appId: "1:1095154488968:web:c5123248b9c65871e8313d"
+  apiKey: "AIzaSyAB0ETykO3ucp1ELm7AlqRFes2xfxP5eEQ",
+  authDomain: "class-8-7502d.firebaseapp.com",
+  databaseURL: "https://class-8-7502d.firebaseio.com",
+  projectId: "class-8-7502d",
+  storageBucket: "class-8-7502d.appspot.com",
+  messagingSenderId: "212265249323",
+  appId: "1:212265249323:web:7a473911ddc1a49775586e"
 };
 
 firebase.initializeApp(firebaseConfig);
